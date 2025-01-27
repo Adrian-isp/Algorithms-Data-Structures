30fb66fe2126b3e8cb09e89e72607b95c5d92e91
Adrian-isp <adrianrosu5400@gmail.com>
2025-01-27 19:19:13 +0200

    basic directory

---
+++ bubble_sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+void bubble_sort(int array[], int size){
+
+    int ok;
+    //use a loop that runs as long as elements have still been swapped
+    do{
+        ok = 1;
+        //go through all elements for every loop
+        for(int i = 0; i < size-1; i++){
+            //check if the elements are in DESCENDING order:
+            if(array[i] > array[i+1]){
+                //if so, swap variables using a temporary variable:
+                int temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+
+                //change the ok variable accordingly
+                ok = 0;
+            }
+        }
+        
+    }while(ok == 0);
+}
+
+int main(){
+    int n;
+    int v[100];
+
+    std::cout << "Type the amount of numbers in the array: ";
+    std::cin >> n;
+
+    std::cout << "Type "<< n << " numbers: ";
+
+    for(int i = 0; i<n; i++){
+        std::cin >> v[i];
+    }
+
+    bubble_sort(v, n);
+
+    for(int i = 0; i<n; i++){
+        std::cout << v[i] << " ";
+    }
+
+    return 0;
+}
