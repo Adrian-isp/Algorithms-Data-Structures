89a3cc6bf8ccb24ed459477ce3f5e6a6b5e45db6
Adrian-isp <adrianrosu5400@gmail.com>
2025-02-02 18:50:29 +0200

    ready for linear video

---
+++ search/linear_search.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+
+void makeRandomArray(int arr[], int &size) {
+    srand(0);
+
+    size = 10 + rand() % 10;
+
+    for(int i = 0; i < size; i++){
+        arr[i] = rand() % 100;
+    }
+}
+void displayArray(int arr[], int start, int end){
+    for(int i = start; i <= end; i++){
+        std::cout << arr[i] << " ";
+    }
+    std::cout << '\n';
+}
+
+int main(){
+    int array[20];
+    int size;
+    makeRandomArray(array, size);
+
+    
+
+    return 0;
+}
