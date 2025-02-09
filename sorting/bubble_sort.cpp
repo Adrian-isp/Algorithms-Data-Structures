a8a6608158631a27969f1f92f0a60934a525d1af
Adrian-isp <adrianrosu5400@gmail.com>
2025-02-09 23:33:10 +0200

    all of the bubble sort for that stupid video

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -30,8 +30,20 @@ int main(){
     makeRandomArray(array, size);
     displayArray(array, size);
 
-    
+    int swapped;
+    do{
+        swapped = false;
+        for(int i = 0; i < size - 1; i++){
+            if(array[i] > array[i+1]){
+                int temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
 
+                swapped = true;
+            }
+        }
+        displayArray(array, size);
+    }while(swapped);
 
     return 0;
 }
