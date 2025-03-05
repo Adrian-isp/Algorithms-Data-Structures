b11c0fab7b8d5dda883fad267623b49ce23530ba
Adrian-isp <adrianrosu5400@gmail.com>
2025-03-05 23:01:34 +0200

    c++ vectors

--- data_structures/arrays/vectors.cpp
+++ data_structures/arrays/vectors.cpp
@@ -5,19 +5,30 @@
 // They are part of the standard template library (STL)
 
 int main() {
-    //vector declaration:
-    std::vector<int> nums = {2, 5, 88};
-
-    // appending elements to vectors:
-    nums.push_back(22);
-    nums.push_back(5234);
+    //vector declaration and initialisation:
+    std::vector<int> numvector = {2, 5, 88};
 
     // can be indexed just like regular arrays
-    for(int num : nums) {
+    for(int num : numvector) {
         std::cout << num << " ";
     }
     // or use the at method:
-    std::cout << "\n1st element: " << nums.at(0);
+    std::cout << "\n1st element: " << numvector.at(0);
+    
+    // appending elements to vectors:
+    numvector.push_back(22);
+    numvector.push_back(5234);
+    // popping items at the end of the vector:
+    numvector.pop_back();
+
+    // size attribute of vector:
+    std::cout << "\nsize of vector: " << numvector.size() << '\n';
+
+    // delete the second element of the vector (using an iterator)
+    numvector.erase(numvector.begin() + 1);
+
+    // destroys the vector
+    numvector.clear();
 
     return 0;
 }
