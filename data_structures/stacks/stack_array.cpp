d5510dd0fbdeba756c84a79ec4172320f8a614e0
Adrian-isp <adrianrosu5400@gmail.com>
2025-03-07 18:02:05 +0200

    stack array completion

--- data_structures/stacks/stack_array.cpp
+++ data_structures/stacks/stack_array.cpp
@@ -8,17 +8,40 @@
 // used for storing data that needs to be retrieved
 // in the order it was placed
 
+// for example: undo/redo functionality on apps
+// Depth first search, parsing, backtracking
+
 class Stack {
     int arr[50];
-    int top = 0;
 public:
+    // in this case, the top value 
+    // is equal the size of the stack
+    int top = 0;
+
     void push(int value) {
         arr[top] = value;
         top++;
     }
     int pop() {
-        top--;
-        return arr[top];
+        try{
+            if(top <= 0)
+                throw "empty stack";
+            top--;
+            return arr[top];
+        }
+        catch (const char *exc) {
+            std::cout << "stack is empty and cannot be popped\n";
+            return 0;
+        }
+    }
+    void display() {
+        std::cout << "values in the stack:\n";
+
+        std::cout << '[';
+        for(int i = top-1; i > 0; i--) {
+            std::cout << arr[i] << ',';
+        }
+        std::cout << arr[0] << "]\n";
     }
 };
 
@@ -32,6 +55,7 @@ int main() {
         std::cout << "Select an action:\n"
                   << "'a' to push to stack\n"
                   << "'p' to pop from stack\n"
+                  << "'d' to display\n"
                   << "'x' to exit\n";
 
         char response;
@@ -50,9 +74,15 @@ int main() {
                 value = stack.pop();
                 std::cout << "Item "<< value << " was taken out of the stack\n\n";
                 break;
+
+            case 'd':
+                stack.display();
+                break;
+
             case 'x':
                 running = false;
                 break;
+
             default:
                 std::cout << "Invalid. Choose again\n\n";
                 break;
