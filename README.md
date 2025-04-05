2b018a3f07f2ad5289cc2f1174c329ee0dc71ec9
Adrian-rospx <adrianrosu5400@gmail.com>
2025-04-05 23:55:04 +0300

    finish: working tree data structure!

--- README.md
+++ README.md
@@ -26,6 +26,9 @@ Created on the 3rd of March 2025.
 
 ## Data Structures:
 
+#### Trees:
+- `Binary Tree implementation` - Apr-05
+
 #### Hash maps:
 
 - `Unordered Map (C++)` - Mar-12
--- data_structures/trees/tree.cpp
+++ data_structures/trees/tree.cpp
@@ -9,43 +9,87 @@
 // useful for file systems or decision trees
 
 using std::string;
-using std::unique_ptr;
+using std::shared_ptr;
+using std::weak_ptr;
+
+// tree nodes with two branches:
+struct Node {
+    string data;
+    shared_ptr<Node> left;
+    shared_ptr<Node> right;
+    // constructor:
+    Node(string data) {
+        this->data = data;
+    }
+};
 
 class Tree {
-    // tree nodes with two branches:
-    struct Node {
-        string data;
-        unique_ptr<Node> left;
-        unique_ptr<Node> right;
-    };
 public:
     // The root node of the tree
-    unique_ptr<Node> root;
+    shared_ptr<Node> root;
     // The selected node of the tree
-    unique_ptr<Node> selected;
+    shared_ptr<Node> selected;
 
     // basic attributes:
-    int size;
-    int depth;
+    int tree_size;
     
     // Initialise the tree
     Tree(string rootData) {
-        root = std::make_unique<Node>();
-        size = 1;
-        depth = 1;
-
-        root->data = rootData;
-        root->left = nullptr;
-        root->right = nullptr;
+        root = std::make_shared<Node>(rootData);
+        // initialise selected
+        selected = root;
+        
+        tree_size = 1;
     }
 
-    // Add a left Node
+    void move_to_root() {selected = root;}
+    void move_to_left() {if(selected->left!=nullptr) selected = selected->left;}
+    void move_to_right() {if(selected->right!=nullptr) selected = selected->right;}
+
+    // Create a left Node
+    void add_left(string str) {
+        if(selected->left != nullptr) {
+            std::cout << "left pointer not empty!\n";
+            return;
+        }
+        selected->left = std::make_shared<Node>(str);
+        selected = selected->left;
+        
+        tree_size++;
+    }
+    // Create a right Node
+    void add_right(string str) {
+        if(selected->right != nullptr) {
+            std::cout << "right pointer not empty!\n";
+            return;
+        }
+        selected->right = std::make_shared<Node>(str);
+        selected = selected->right;
+        
+        tree_size++;
+    }
+
+    // print the value inside the current tree node
+    void print_node() {
+        std::cout << selected->data << '\n';
+    }
+
 };
 
 int main() {
     Tree tree("Root of tree");
+    tree.print_node();
 
-    std::cout << tree.root->data;
+    // add two branches
+    tree.add_left("branch A");
+    tree.move_to_root();
+    tree.add_right("branch B");
+
+    // print the branches by moving manually
+    tree.move_to_root();
+    tree.move_to_left(); tree.print_node();
+    tree.move_to_root();
+    tree.move_to_right(); tree.print_node();
 
     return 0;
 }
